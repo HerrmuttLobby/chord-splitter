99f0f6e9a2658dc14ef45a6703834a48d7c3e668
hems <dr4gon@dr4gon.local>
2012-06-20 03:30:37 +0100

    improving syntax removing unused code

--- chordsplitter.js
+++ chordsplitter.js
@@ -1,70 +1,55 @@
-/* Herrmutt Lobby • Chord Splitter JS 0.6 */
+/* Herrmutt Lobby • Chord Splitter JS 0.4 */
 /* (c) Herrmutt Lobby 2012 • herrmuttlobby.com */
-/* This software is distributed under a  */
+/* This code is distributed under a Creative Commons : Attribution, Share Alike, No-commercial Licence */
+/* INPUT : list message starting with note then a note number and velocity ( note noteNbr velocity ) or "reset" message to reset the chord */
+/* OUTPUT : quick series of ordered note tuples ( [index, noteNbr, velocity, nbrOfNoteInTheChord] ) */
 /* MADE TO BE USED WITHIN the JS object of MAX4LIVE or MAX/MSP or in PureData with the jj object of the PDJ external (http://www.le-son666.com/software/pdj/) */
 
 /* CONFIG (should be done with message through an inlet */
 
 groupSend = false; // choose between pushing note once by once through the outlet, or all in a big tuple
-debug = false; // put in debug mode
+DEBUG    = false; // put in debug mode
 
 /* CODE */
 
-quicksort = function(input){ /* quick sort function */
-  if(input.length <= 1)
+/* quick sort function */
+quicksort = function( input ){
+  if(input.length <= 1) return input;
+
+  var pivot   = input.splice(0, 1);
+  var less    = [];
+  var greater = [];
+  
+  for(i = 0; i < input.length; i++)
   {
-    return input;
-  }else
-  {
-    var pivot = input.splice(0, 1);
-    var less = [];
-    var greater = [];
-    var output = [];
+    x = input[i];
     
-    for (i = 0; i < input.length; i++)
-    {
-      x = input[i];
-      
-      if (x[0] <= pivot[0][0])
-      {
-        less.push(x);
-      }else{
-        greater.push(x);
-      }
-    }
-    
-    return output.concat(quicksort(less), pivot, quicksort(greater));
+    if (x[0] <= pivot[0][0])
+      less.push(x);
+    else
+      greater.push(x);
   }
+  
+  return [].concat(quicksort(less), pivot, quicksort(greater));
 }
 
-var splitter = new Object;
+var splitter = {
+   groupSend : groupSend
+  ,chord     : []
+  ,addStatus : false
+  ,outChord  : []
+}
 
-splitter.groupSend = groupSend;
-splitter.debug = debug;
-splitter.chord = [];
-splitter.addStatus = false;
-splitter.outChord = [];
+splitter.newNote = function(note){
+  if(DEBUG) post("newnote" + note);
 
-//*NOTE IN*//
-//* When a note is received we check if the note is a note on or a note off and route them accordingly. *//
-splitter.newNote = function(note)
-{
-  if(splitter.debug == true)
-  {
-    post("newnote " + note); 
-  }
-
-  if(note[1] === 0)
-  {
-    this.delNote(note);
-  }else
-  {
+  if( note[1] ) // if velocity is greater than 0
     this.addNote(note);
-  }
+  else
+    this.delNote(note);
 }
 
-//*NOTE ON*//
-//* When a note on (vel not equal to 0) is received, the note is added to the buffer, then the buffer is sorted and sent to output *//
+
 splitter.addNote = function(note){ /* add a note to the chord */
   this.addStatus = true;
   this.chord.push(note);
@@ -73,9 +58,7 @@ splitter.addNote = function(note){ /* add a note to the chord */
   this.output42();
 }
 
-//*NOTE OFF*//
-//* When a note with a velocity of 0 (note off) is received, the corresponding note on is removed from the buffer and the note off is send with the channel number of the corresponding note on, then the buffer is sent to output *//
-splitter.delNote = function(note){ 
+splitter.delNote = function(note){
   this.addStatus = false;
   
   for (i = 0; i < this.chord.length; i++)
@@ -90,53 +73,55 @@ splitter.delNote = function(note){
   this.output42();
 }
 
-//*RESET*//
-//* On a reset message a note off is sent to the output and the buffer is cleared. *//
-splitter.reset = function() 
-{
-  for (i = 0; i< this.chord.length; i++)
-  {
-    outlet(0, [this.chord[i][3], this.chord[i][0], 0, this.chord.length-1]);
-  }
+splitter.reset = function(){
   this.chord = [];
 }
 
-//*OUTPUT*//
-//* When we need to send the whole buffer (after a note on or off) we trig this output function *//
+/* perhaps outputs the whole chord */
 splitter.output42 = function(){
-  this.outChord = []; //init the big tuple wich will be sent if we asked for
+  this.outChord = [];
   
-  for (i = 0; i < this.chord.length; i++){
-    
-    if(this.chord[i][3] != i+1 && this.chord[i][3]) // if the note channel has been shifted from previous position
-    {
-      outlet(0, [this.chord[i][3], this.chord[i][0], 0, this.chord.length-1]); // send note off
-      this.chord[i][2] = false;
-    }
-    
-    this.chord[i][3] = i+1; // change the channel number
-    
-    if(splitter.groupSend == false){ // send note on
-      if(this.chord[i][2] == false)
-      {
-          outlet(0, [this.chord[i][3], this.chord[i][0], this.chord[i][1], this.chord.length]);
-          this.chord[i][2] = true;
-      }
-    }
-    else{ //populate the big tuple
-      this.outChord.push([this.chord[i][3], this.chord[i][0], this.chord[i][1], this.chord.length]);
-    }
+  for (i = 0; i < this.chord.length; i++){ // check all notes
+        if(this.chord[i][3] != i+1) // if note index changed
+        {
+            // send note off
+            outlet(0, [this.chord[i][3], this.chord[i][0], 0, this.chord.length-1]);
+            // update note index
+            this.chord[i][3] = i+1;
+            // send note on through the new chanel
+            outlet(0, [this.chord[i][3], this.chord[i][0], this.chord[i][1], this.chord.length]);
+        }
   }
   
-  if(splitter.groupSend == true){ /* Sending the big tuple if asked for */
+  if(splitter.groupSend == true){ /* SENDING THE WHOLE CHORD AS A BIG TUPLE CONTAINING TUPLES */
     outlet(0, outChord);
-    outlet(1, this.chord.length);
   }
+}
+
+splitter.outLogic = function(i)
+{
+    var out = [i+1, this.chord[i][0], this.chord[i][1], this.chord.length];
+    
+    if(DEBUG)
+        post('i = ' + ( i + 1 ) + ' :: ' + this.chord[i]);
+    
+    /* SENDING TUPLE ONE AT A TIME */
+    if(splitter.groupSend == false){ 
+        
+        if(this.chord[i][2] == false){
+            this.chord[i][2] = true;
+            this.chord[i].push(i+1);
+            outlet(0, out);
+        }
+
+    }
+    
+    this.outChord.push(out);
 }
 
 /* MAIN */
 
-inlets = 1; // number of inlets
+inlets  = 1; // number of inlets
 outlets = 2; // number of outlets
 
 function note(note, vel)
@@ -144,9 +129,9 @@ function note(note, vel)
   splitter.newNote([note, vel, false]);
 }
 
-function list(info, note, val)
+function list(info, note, vel)
 {
-  splitter.newNote([note, vel, false]); // 0=note number, 1=velocity, 2=has been played, 3=previous index
+  splitter.newNote([note, vel, false]);
 }
 
 function reset()
